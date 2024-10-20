c7f758a2a9ffb2de19179df4367cb57900e21ca5
Nguyen Thanh Nam <nvthanhnam121@gmail.com>
2024-10-20 11:41:29 +0700

    loverain

---
+++ script.js
@@ -0,0 +1,112 @@
+function rain() {
+  let cloud = document.querySelector(".cloud");
+  let e = document.createElement("div");
+
+  e.classList.add("drop");
+  cloud.appendChild(e);
+
+  let left = Math.floor(Math.random() * 350);
+  let size = Math.round() * 1.5;
+  let duration = Math.random() * 1;
+
+  e.innerText = "🩷";
+  e.style.left = left + "px";
+  e.style.fontSize = 0.5 + size + "em";
+  e.style.animationDirection = 1 + duration + "s";
+
+  setTimeout(function () {
+    cloud.removeChild(e);
+  }, 2000);
+}
+
+setInterval(function () {
+  rain();
+}, 20);
+
+var i = 0,
+  a = 0,
+  isBackspacing = false,
+  isParagraph = false;
+
+var textArray = [
+  "Hey girl ಥ_ಥ",
+  "I just want to say that",
+  "Every day, I find inspiration in you.",
+  "Your strength and passion motivate me",
+  "to strive for my best.",
+  "Knowing that",
+  "you’re in my life",
+  "pushes me to work harder",
+  "and be a better person.",
+  "I love you so much.",
+];
+
+var speedForward = 100,
+  speedWait = 1000,
+  speedBetweenLines = 1000,
+  speedBackspace = 25;
+
+function typeWriter(id, ar) {
+  var element = document.getElementById(id),
+    aString = ar[a],
+    eHeader = element.querySelector("h1"),
+    eParagraph = element.querySelector("p");
+
+  if (!isBackspacing) {
+    if (i < aString.length) {
+      if (aString.charAt(i) == "|") {
+        isParagraph = true;
+        eHeader.classList.remove("cursor");
+        eParagraph.classList.add("cursor");
+        i++;
+        setTimeout(function () {
+          typeWriter(id, ar);
+        }, speedBetweenLines);
+      } else {
+        if (!isParagraph) {
+          eHeader.textContent += aString.charAt(i);
+        } else {
+          eParagraph.textContent += aString.charAt(i);
+        }
+        i++;
+        setTimeout(function () {
+          typeWriter(id, ar);
+        }, speedForward);
+      }
+    } else if (i == aString.length) {
+      isBackspacing = true;
+      setTimeout(function () {
+        typeWriter(id, ar);
+      }, speedWait);
+    }
+  } else {
+    if (eHeader.textContent.length > 0 || eParagraph.textContent.length > 0) {
+      if (eParagraph.textContent.length > 0) {
+        eParagraph.textContent = eParagraph.textContent.substring(
+          0,
+          eParagraph.textContent.length - 1
+        );
+      } else if (eHeader.textContent.length > 0) {
+        eParagraph.classList.remove("cursor");
+        eHeader.classList.add("cursor");
+        eHeader.textContent = eHeader.textContent.substring(
+          0,
+          eHeader.textContent.length - 1
+        );
+      }
+      setTimeout(function () {
+        typeWriter(id, ar);
+      }, speedBackspace);
+    } else {
+      isBackspacing = false;
+      i = 0;
+      isParagraph = false;
+      a = (a + 1) % ar.length;
+      setTimeout(function () {
+        typeWriter(id, ar);
+      }, 50);
+    }
+  }
+}
+
+typeWriter("output", textArray);
